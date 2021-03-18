2d548074edb4f7ee28644064444635f82a3031d6
MisterEOAK <64244961+MisterEOAK@users.noreply.github.com>
2021-03-17 20:04:26 -0800

    Shows string of BPM

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-pins.onPulsed(DigitalPin.P0, PulseValue.High, () => {
-    basic.showNumber(pins.pulseDuration());
-});
+let time1 = 0
+let delta_t = 0
+let time2 = 0
+let PulseDet = 0
+let pulse_out = 0
+basic.showString("PULS__")
+let counter = 0 
+basic.forever(function () {
+    basic.showNumber(pulse_out)
+    basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+    PulseDet = pins.analogReadPin(AnalogPin.P2)
+})
+basic.forever(function () {
+serial.writeValue("Pulse Diagram", PulseDet)    
+})
+basic.forever(function () {
+ if (PulseDet > 900 && counter == 0) {
+     time2 = input.runningTime()
+     delta_t = time2 - time1
+     time1 = time2
+     counter = 1
+     pulse_out = (60000 -60000 % delta_t) / delta_t
+ }   else if (PulseDet <= 430 && counter == 1) {
+     counter = 0
+ }
+})
